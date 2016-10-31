0a836dba260617e9ffea71d5b0213372ff06b892
Mikerr1111 <miroyal@csumb.edu>
2016-10-31 11:49:05 -0700

    Gives distance between origin and destination
    Gets the Time and duration it takes to travel between the origin and destination entered.

---
+++ distance.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+from geolocation.main import GoogleMaps
+from geolocation.distance_matrix.client import DistanceMatrixApiClient
+
+if __name__ == "__main__":
+    origins = ['Los Angeles', 'New York City']
+    destinations = ['Seaside']
+
+    google_maps = GoogleMaps(api_key='AIzaSyBTfMBsYy9fFaJ-XVhoHIz-VnKiN2DZGpg')
+
+    items = google_maps.distance(origins, destinations).all()  # default mode parameter is const.MODE_DRIVING
+
+    for item in items:
+
+        print('origin: %s' % item.origin)
+        print('destination: %s' % item.destination)
+       #print('km: %s' % item.distance.kilometers)
+       #print('m: %s' % item.distance.meters)
+        print('miles: %s' % item.distance.miles)
+        print('duration: %s' % item.duration)  # it returns str
+        print('duration datetime: %s' % item.duration.datetime)  # it returns datetime
+
+        # you can also get items from duration
+        #print('duration days: %s' % item.duration.days)
+        #print('duration hours: %s' % item.duration.hours)
+        #print('duration minutes: %s' % item.duration.minutes)
+        print('duration seconds: %s' % item.duration.seconds)
+
+    items = google_maps.distance(origins, destinations, DistanceMatrixApiClient.MODE_BICYCLING).all()
+
+    for item in items:
+		
+        print('origin: %s' % item.origin)
+        print('destination: %s' % item.destination)
+        #print('km: %s' % item.distance.kilometers)
+        #print('m: %s' % item.distance.meters)
+        print('miles: %s' % item.distance.miles)
+        print('duration: %s' % item.duration)
+
+    items = google_maps.distance(origins, destinations, DistanceMatrixApiClient.MODE_WALKING).all()
+
+    for item in items:
+        
+        print('origin: %s' % item.origin)
+        print('destination: %s' % item.destination)
+        #print('km: %s' % item.distance.kilometers)
+        #print('m: %s' % item.distance.meters)
+        print('miles: %s' % item.distance.miles)
+        print('duration: %s' % item.duration)
+
+    items = google_maps.distance(origins, destinations, DistanceMatrixApiClient.MODE_TRANSIT).all()
+
+    for item in items:
+        print('Transit')
+        print('origin: %s' % item.origin)
+        print('destination: %s' % item.destination)
+        #print('km: %s' % item.distance.kilometers)
+        #print('m: %s' % item.distance.meters)
+        print('miles: %s' % item.distance.miles)
+        print('duration: %s' % item.duration)
+
+    items = google_maps.distance(origins, destinations, avoid=DistanceMatrixApiClient.AVOID_HIGHWAYS).all()
+
+    for item in items:
+        print('Avoid Highways')
+        print('origin: %s' % item.origin)
+        print('destination: %s' % item.destination)
+        #print('km: %s' % item.distance.kilometers)
+        #print('m: %s' % item.distance.meters)
+        print('miles: %s' % item.distance.miles)
+        print('duration: %s' % item.duration)
+
+    items = google_maps.distance(origins, destinations, avoid=DistanceMatrixApiClient.AVOID_FERRIES).all()
+
+    for item in items:
+        print('Avoid Ferries')
+        print('origin: %s' % item.origin)
+        print('destination: %s' % item.destination)
+        #print('km: %s' % item.distance.kilometers)
+        #print('m: %s' % item.distance.meters)
+        print('miles: %s' % item.distance.miles)
+        print('duration: %s' % item.duration)
+
+    items = google_maps.distance(origins, destinations, avoid=DistanceMatrixApiClient.AVOID_TOLLS).all()
+
+    for item in items:
+        print('Avoid Tolls')
+        print('origin: %s' % item.origin)
+        print('destination: %s' % item.destination)
+        #print('km: %s' % item.distance.kilometers)
+        #print('m: %s' % item.distance.meters)
+        print('miles: %s' % item.distance.miles)
+        print('duration: %s' % item.duration)
