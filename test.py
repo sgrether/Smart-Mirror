c329cf7c1fff967ea99bd0647cc4798830734e82
Scott Grether <sgrether@csumb.edu>
2016-11-07 13:29:08 -0800

    the clock now ticks every second

--- test.py
+++ test.py
@@ -7,24 +7,25 @@ except:
     #python2
     from Tkinter import *
 
+time1 = time.localtime(time.time())
+
 class Application(Frame):
 
     def createTime(self):
-        self.Time = Label(self.top_right, text=time.asctime(self.localtime))
+        self.Time = Label(self.top_right)
         self.Time.pack(side='right')
 
     def createCal(self):
         self.Cal = Text(self.bottom_right,height=8,width=0,background='gray')
-        self.Cal.insert(INSERT, calendar.month(self.localtime[0], self.localtime[1]))
+        self.Cal.insert(INSERT, calendar.month(time1[0], time1[1]))
         self.Cal.pack(side='right',fill='both',expand=True)
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
-        self.localtime = time.localtime(time.time())
 
         self.main_container = Frame(master, background='bisque')
         self.main_container.pack(side='top',fill='both',expand=True)
-        master.minsize(width=500,height=500)
+        master.minsize(width=1000,height=500)
 
         self.top_frame = Frame(self.main_container,background='green')
         self.top_frame.pack(side='top',fill='x',expand=False)
@@ -59,16 +60,16 @@ class Application(Frame):
         self.bottom_left_label = Label(self.bottom_left,text='Bottom Left')
         self.bottom_left_label.pack(side='left')
 
-def tick(app):
+def tick():
+    global time1
     time2 = time.localtime(time.time())
-    if time2 != app.localtime:
-        app.localtime = time2
-        app.Time.config(text=time.asctime(app.localtime))
-    app.Time.after(200,tick(app))
+    if time2 != time1:
+        time1 = time2
+        app.Time.config(text=time.asctime(time1))
+    app.Time.after(200, tick)
 
 root = Tk()
 root.title('Test.py')
 app = Application(root)
-#tick(app)
-
-app.mainloop()
+tick()
+root.mainloop()
