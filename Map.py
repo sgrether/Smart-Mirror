1d84acb96f1cf3d49960c7c16f0f2387b6473c93
Mikerr1111 <miroyal@csumb.edu>
2016-11-09 11:31:34 -0800

    Add files via upload

--- Map.py
+++ Map.py
@@ -3,6 +3,7 @@ from PIL import ImageTk, Image
 import os
 
 root = Tk()
+root.title("Map of Seaside")
 img = ImageTk.PhotoImage(Image.open("Seaside.png"))
 panel = Label(root, image = img)
 panel.pack(side = "bottom", fill = "both", expand = "yes")
